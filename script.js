98a3ae8005b55fa57076054dfca165b280c1cd3b
jithu1703 <jithups252@gmail.com>
2023-12-16 15:40:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var typed = new Typed(".typing", {
+    strings: [
+      "Front End Developer",
+      "Web Designer",
+      "PHysics Graduate",
+      "Tech Enthusisast",
+      "Author"
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
